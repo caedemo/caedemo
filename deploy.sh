44a63445c8e0ec142e88261e28ce215e3d87c75a
Compass Agile Enterprise - SaaS Demo <caedemo@mailismagic.com>
2014-01-31 15:15:51 -0500

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 # add all files
 echo "Staging all changes"
-git add .
+git add . &> ../deploy.log
 
 # commit
 echo "Creating an auto commit"
-git commit -m 'auto commit'
+git commit -m 'auto commit' &> ../deploy.log
 
 # push
 echo "Pushing to GitHub"
-git push origin master
+git push origin master &> ../deploy.log
 
 # wait a few seconds for 
 echo "Sleeping for 5 seconds to wait for changes to be available on GitHub"
@@ -20,10 +20,10 @@ echo "Pulling changes to AWS"
 
 # pull changes to aws
 # and restart nginx
-ssh ubuntu@54.84.82.146 "cd ~/sites/caedemo; git pull origin master"
+ssh ubuntu@54.84.82.146 "cd ~/sites/caedemo; git pull origin master" &> ../deploy.log
 
 echo "Restarting nginx on AWS"
 
-ssh ubuntu@54.84.82.146 "sudo service nginx restart"
+ssh ubuntu@54.84.82.146 "sudo service nginx restart" &> ../deploy.log
 
 echo "Done!"
