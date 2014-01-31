15018fd2a0612a28b04b3af041f2961b96d23f17
Ubuntu <ubuntu@ip-172-31-47-139.ec2.internal>
2014-01-31 17:18:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caedemo::Application.config.secret_token = 'a77aa80afe30705d6cfd3cbd12c00702e28e7fe9298a5ae25f549709dddc3ef941a9a1bed109668b3f68719df7716b3da2d8f5404d322de97b0cdf1eeecf2fb3'
